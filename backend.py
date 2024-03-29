a5f7d151882855db6e09f9f3c65a27f14544dc46
Ramesh86-TurBo <pairamesh2002@gmail.com>
2024-03-29 11:49:48 +0530

    audio download function added

--- backend.py
+++ backend.py
@@ -10,7 +10,7 @@ from googletrans import Translator
 # Create the Flask instance and pass the Flask constructor the path of the correct module
 app = Flask(__name__)
 
-# program for transcribing and translating the youtube audio to text
+# TRANSCRIBING AND TRANSLATING SCRIPT
 
 # Function to check and select the appropriate device (CPU or CUDA)
 def check_device():
@@ -38,10 +38,14 @@ def download_audio(url, output_path, filename):
 
         print(f"Audio downloaded to {output_path}/{filename}")
         return True
+    
     except Exception as e:
+        
         print(f"Error downloading audio: {e}")
         return False
-    
+
+ 
+# ROUTES
 
 # home route
 @app.route('/', methods = ['GET', 'POST'])
@@ -51,8 +55,11 @@ def home():
          name = "Invalid"
       else:
          name = request.form['name']
+
+         # Download audio
+         audio_downloaded = download_audio(name, "static", "audio.mp3")
          
-   return render_template('index.html', title = 'home', name = name, device = check_device())
+   return render_template('index.html', title = 'home', name = name, device = check_device(), audio_downloaded = audio_downloaded)
 
 
 # Start with flask web app with debug as True only if this is the starting page
